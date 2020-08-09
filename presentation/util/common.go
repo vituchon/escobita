34d073a4f043a9ed5207c5b5d675b475b5dcffea
Vituchon <victor@omniasalud.com>
2020-08-09 20:40:29 -0300

    added common functions place for presentation

--- presentation/util/common.go
+++ presentation/util/common.go
@@ -1,3 +1,15 @@
 // The presentation layer contains all resources concerned with creating an application interface
 // This file contains common code that apply to any type of interface (CLI, native GUI or web GUI or whatever..)
-package presentation
+package util
+
+import (
+	"os"
+)
+
+func FileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
