e4a5bf5e6d3e0e7e9af8d53daba8caab935b6c3b
Vituchon <victor@omniasalud.com>
2020-08-16 16:59:50 -0300

    implemented first lobby ui

--- presentation/web/assets/js/ts/controllers/lobby.ts
+++ presentation/web/assets/js/ts/controllers/lobby.ts
@@ -1,10 +1,27 @@
 /// <reference path='../app.ts' />
+/// <reference path='../services/services.ts' />
 
 module Lobby {
   class Controller {
-    constructor(private $state: ng.ui.IStateService) {
+
+    public games: Games.Game[]
+
+    constructor(private $state: ng.ui.IStateService, private gamesService: Games.Service) {
+      this.games = [];
+    }
+
+    public createGame(game: Api.Game) {
+      this.gamesService.createGame(game).then((createdGame) => {
+        this.games.push(createdGame)
+      })
+    }
+
+    public updateGameList() {
+      this.gamesService.getGames().then((games) => {
+        this.games = games
+      })
     }
   }
 
-  escobita.controller('LobbyController', ['$state', Controller]);
+  escobita.controller('LobbyController', ['$state', 'GamesService', Controller]);
 }
