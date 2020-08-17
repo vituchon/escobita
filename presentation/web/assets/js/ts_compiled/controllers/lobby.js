2844a8e53dfe56e0f5a04a9c54f9945339e7ae03
Vituchon <victor@omniasalud.com>
2020-08-17 10:27:53 -0300

    grgrg

--- presentation/web/assets/js/ts_compiled/controllers/lobby.js
+++ presentation/web/assets/js/ts_compiled/controllers/lobby.js
@@ -1,11 +1,26 @@
 /// <reference path='../app.ts' />
+/// <reference path='../services/services.ts' />
 var Lobby;
 (function (Lobby) {
     var Controller = (function () {
-        function Controller($state) {
+        function Controller($state, gamesService) {
             this.$state = $state;
+            this.gamesService = gamesService;
+            this.games = [];
         }
+        Controller.prototype.createGame = function (game) {
+            var _this = this;
+            this.gamesService.createGame(game).then(function (createdGame) {
+                _this.games.push(createdGame);
+            });
+        };
+        Controller.prototype.updateGameList = function () {
+            var _this = this;
+            this.gamesService.getGames().then(function (games) {
+                _this.games = games;
+            });
+        };
         return Controller;
     }());
-    escobita.controller('LobbyController', ['$state', Controller]);
+    escobita.controller('LobbyController', ['$state', 'GamesService', Controller]);
 })(Lobby || (Lobby = {}));
