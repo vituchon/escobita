94a3d4a93836c65f98e8af9fc3aac6be9d9ff809
Vituchon <victor@omniasalud.com>
2020-08-22 22:09:27 -0300

    removed unused

--- services/app.go
+++
@@ -1,5 +0,0 @@
-// Contains code designed to be used regardless the client-interface.
-// Another valid file name would be sync.go as it cointains code regarding synchronization affairs
-package services
-
-const AppVersion = "1.0.0"
