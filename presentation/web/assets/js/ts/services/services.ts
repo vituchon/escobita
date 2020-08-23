4e9e5c0a7c7d972631f6d38d5b24b92bf440c663
Vituchon <victor@omniasalud.com>
2020-08-22 22:14:14 -0300

    enhaced services to work with players

--- presentation/web/assets/js/ts/services/services.ts
+++ presentation/web/assets/js/ts/services/services.ts
@@ -39,8 +39,8 @@ namespace Games {
       });
     }
 
-    getGameById(gamesId: number): ng.IPromise<Game[]> {
-      return this.$http.get<Game[]>(`/api/v1/games/${gamesId}`).then((response) => {
+    getGameById(gamesId: number): ng.IPromise<Game> {
+      return this.$http.get<Game>(`/api/v1/games/${gamesId}`).then((response) => {
         return response.data;
       });
     }
@@ -48,9 +48,6 @@ namespace Games {
     createGame(game: Game): ng.IPromise<Game> {
       return this.$http.post<Game>(`/api/v1/games`,game).then((response) => {
         return response.data
-      }).catch((err) => {
-        console.log(err)
-        throw err
       })
     }
 
@@ -71,8 +68,80 @@ namespace Games {
   escobita.service('GamesService', ['$http', '$q', Service]);
 }
 
-var gameService: Games.Service;
-// TODO : remove when dev finish
-escobita.run(['GamesService', (gs: Games.Service) => {
-  gameService = gs
-}])
+
+namespace Players {
+
+  export interface Player extends Api.Player {
+  }
+
+  export namespace Periods {
+    const dayInSeconds = 24 * 60 * 60;
+    const weekInSeconds = dayInSeconds * 7;
+
+    //  Well know values
+    export enum Values {
+      Daily = dayInSeconds,
+      Weekly = weekInSeconds,
+     };
+
+     // common legend to display for each value, it shall be accessed via the well know values
+    export const labels = Object.freeze({
+      [Values.Daily] : "Diaria",
+      [Values.Weekly] : "Semanal"
+    });
+
+    export function getPeriodDescription(period: Periods.Values): string {
+      return (<any>Periods.labels)[period]
+    }
+  }
+
+  export class Service {
+    constructor(private $http: ng.IHttpService, private $q: ng.IQService) {
+    }
+
+    getPlayers(): ng.IPromise<Player[]> {
+      return this.$http.get<Player[]>(`/api/v1/players`).then((response) => {
+        return response.data;
+      });
+    }
+
+    getPlayerById(playersId: number): ng.IPromise<Player> {
+      return this.$http.get<Player>(`/api/v1/players/${playersId}`).then((response) => {
+        return response.data;
+      })/*.catch((err) => {
+        if (err.status === 404) { // not found: there is no player with the given id
+          return null
+        } else {
+          return err
+        }
+      })*/
+    }
+
+    getClientPlayer(): ng.IPromise<Player> {
+      return this.$http.get<Player>(`/api/v1/player`).then((response) => {
+        return response.data;
+      })
+    }
+
+    createPlayer(player: Player): ng.IPromise<Player> {
+      return this.$http.post<Player>(`/api/v1/players`,player).then((response) => {
+        return response.data
+      })
+    }
+
+    updatePlayer(player: Player): ng.IPromise<Player> {
+      return this.$http.put<Player>(`/api/v1/players/${player.id}`,player).then((response) => {
+        return response.data
+      })
+    }
+
+    deletePlayer(player: Player): ng.IPromise<any> {
+      return this.$http.delete<any>(`/api/v1/players/${player.id}`).then((_) => {
+        return null;
+      })
+    }
+
+  }
+
+  escobita.service('PlayersService', ['$http', '$q', Service]);
+}
