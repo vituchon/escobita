3e8064068cd0e7853459fb975757801d5c8b6304
Vituchon <victor@omniasalud.com>
2020-08-28 13:02:41 -0300

    update model

--- presentation/web/assets/js/ts/api-model.ts
+++ presentation/web/assets/js/ts/api-model.ts
@@ -1,15 +1,76 @@
+/// <reference path='./third_party_definitions/_definitions.ts' />
 
 namespace Api {
-  export interface Game {
-    id?: number;
-    match: any;
-    players: Player[];
+
+  export interface Card {
+    id: number;
+    suit: number;
+    rank: number;
   }
 
   export interface Player {
     id?: number;
     name: string;
   }
+
+  export interface Match {
+    players?: Player[];
+    actionsByPlayer: ActionsByPlayer;
+    playerAction: any[];
+    matchCards: MatchCards;
+    firstPlayerIndex: number;
+    roundNumber: number;
+  }
+
+  export interface Game {
+    id?: number;
+    name: string;
+    matchs?: Match[];
+    players?: Player[];
+    scorePerPlayer?: ScorePerPlayer;
+    currentMatch?: Match;
+  }
+
+  export interface ScorePerPlayer extends _.Dictionary<number> { // TODO : rename to ScoreByPlayer
+    [name:string]: number;
+  }
+
+  export interface ActionsByPlayer extends _.Dictionary<PlayerAction> {
+    [name:string]: PlayerAction;
+  }
+
+  interface BasePlayerAction {
+    player: Player;
+  }
+
+  export interface MatchCards {
+    board?: Card[];
+    left: Card[];
+    perPlayer: MatchCardsByPlayer; // TODO : rename to byPlayer
+  }
+
+  export interface MatchCardsByPlayer extends _.Dictionary<PlayerMatchCards> {
+    [name:string]: PlayerMatchCards;
+  }
+
+  export interface PlayerMatchCards {
+    taken: Card[];
+    hand: Card[];
+  }
+
+  export interface PlayerTakeAction extends BasePlayerAction {
+    boardCards: Card[];
+    handCard: Card;
+    isEscobita: boolean;
+  }
+
+  export interface PlayerDropAction extends BasePlayerAction  {
+    player: Player;
+    handCard: Card;
+  }
+
+  export type PlayerAction = PlayerTakeAction | PlayerDropAction
+
 
   export interface Message {
     id?: number;
@@ -18,4 +79,6 @@ namespace Api {
     text: string;
     created?: number;
   }
+
+
 }
