2b153559c9903ac0d8d12565a692b57caccd6a07
Vituchon <victor@omniasalud.com>
2020-08-29 10:20:24 -0300

    added card directive

---
+++ presentation/web/assets/js/ts/directives/_directives.d.ts
@@ -0,0 +1,1 @@
+/// <reference path='./card.ts' />
---
+++ presentation/web/assets/js/ts/directives/card.ts
@@ -0,0 +1,33 @@
+/// <reference path='../app.ts' />
+/// <reference path='../api-model.ts' />
+
+namespace Cards {
+
+  /** Angular directive 'card': Render a card.
+  *
+  * Example usage (in HTML template):
+  *  <card ng-model="identifier_1"/>
+  *
+  * @ng-model: Reference to an object of type "Api.Card"
+  */
+  escobita.directive('card', () => {
+    return {
+      restrict: 'E',
+      require: 'ngModel',
+      scope: {
+        card: "=ngModel",
+      },
+      template: `
+        <div class="card-container">
+          <div class="lightText" style="font-size:12px">Palo:&nbsp;</div>
+          <div>{{::card.suit}}&nbsp;</div>
+          <div class="lightText" style="font-size:12px">Número:&nbsp;</div>
+          <div>{{::card.rank}}</div>
+        </div>`,
+      link: function ($scope: ng.IScope, $element: JQuery, attrs: ng.IAttributes, ngModel: ng.INgModelController) {
+      }
+    }
+
+  });
+
+};
