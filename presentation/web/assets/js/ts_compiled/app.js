fcb0f099b3e6b873823e93057a6edbe6d5ae3561
Vituchon <victor@omniasalud.com>
2020-10-03 13:07:18 -0300

    updated code

--- presentation/web/assets/js/ts_compiled/app.js
+++ presentation/web/assets/js/ts_compiled/app.js
@@ -1,4 +1,5 @@
 /// <reference path='./third_party_definitions/_definitions.ts' />
+/// <reference path='./util.ts' />
 var escobita = angular.module('escobita', ['ui.router']);
 var App;
 (function (App) {
@@ -37,7 +38,6 @@ var App;
     ;
     escobita.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', setup]);
 })(App || (App = {}));
-// TODO : move to presentation/web/assets/js/ts/directives and make proper inclusion
 escobita.directive('loading', [function () {
         return {
             restrict: 'E',
