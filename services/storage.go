75b4c5b58d983044826c76bdb462d4edac92d67b
Vituchon <victor@omniasalud.com>
2020-10-10 20:31:27 -0300

    augmented doc

--- services/storage.go
+++ services/storage.go
@@ -4,6 +4,6 @@ package services
 // Dev notes: THe sync methods should be independent of filepaths (local) or urls (remotes), maybe an struct can be build at
 // app boot time using proper config file.
 // Example:
-// dbSyncStateStore = services.NewDbSyncStateStore(config)
+// dbSyncStateStore = services.NewDbSyncStateStore(config), config refers to a config intrisic to services (not to model!)
 // dbSyncStateStore.Load() error
 // dbSyncStateStore.Save() error
