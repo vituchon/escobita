baa4d66c148f8d8a44108bfb74c3ac33f5ec0059
Vituchon <victor@omniasalud.com>
2023-10-13 15:41:02 -0300

    enhaced testing

--- util/arrays.go
+++ util/arrays.go
@@ -3,85 +3,7 @@ package util
 // Generates an array containing all possible permutations regarding element's order
 // chatgpt states: Keep in mind that the number of permutations grows rapidly with the size of the input array, so this function may not be practical for very large input arrays due to the combinatorial explosion of possibilities.
 // so the length of the input array should not be larger than 10, as 10! = 3628800
-func GeneratePermutations_v1a[T any](array []T) [][]T {
-	if len(array) == 1 {
-		return [][]T{array}
-	} else {
-		combinations := [][]T{}
-		for index, value := range array {
-			others := deepCopySlice(array[0:index])
-			others = append(others, array[index+1:len(array)]...)
-			subcombinations := GeneratePermutations_v1a(others)
-			for _, subcombination := range subcombinations {
-				combination := append([]T{value}, subcombination...)
-				combinations = append(combinations, combination)
-			}
-		}
-		return combinations
-	}
-}
-
-func GeneratePermutations_v1b[T any](array []T) [][]T {
-	if len(array) == 1 {
-		return [][]T{array}
-	} else {
-		combinations := [][]T{}
-		for index, value := range array {
-			others := shallowCopySlice(array[0:index])
-			others = append(others, array[index+1:len(array)]...)
-			subcombinations := GeneratePermutations_v1b(others)
-			for _, subcombination := range subcombinations {
-				combination := append([]T{value}, subcombination...)
-				combinations = append(combinations, combination)
-			}
-		}
-		return combinations
-	}
-}
-
-func deepCopySlice[T any](original []T) []T {
-	// Create a new slice with the same length as the original.
-	copied := make([]T, len(original))
-
-	// Copy each element from the original slice to the new slice.
-	for i, v := range original {
-		copied[i] = v
-	}
-
-	return copied
-}
-
-func shallowCopySlice[T any](original []T) []T {
-	copied := make([]T, len(original), len(original))
-	copy(copied, original)
-	return copied
-}
-
-func GeneratePermutations_v2[T any](array []T) [][]T {
-	if len(array) == 1 {
-		return [][]T{array}
-	} else {
-		combinations := [][]T{}
-		for index, value := range array {
-			others := []T{}
-			for i := 0; i < index; i++ {
-				others = append(others, array[i])
-			}
-			for i := index + 1; i < len(array); i++ {
-				others = append(others, array[i])
-			}
-			subcombinations := GeneratePermutations_v2(others)
-			for _, subcombination := range subcombinations {
-				combination := append([]T{value}, subcombination...)
-				combinations = append(combinations, combination)
-			}
-		}
-		return combinations
-	}
-}
-
-// this is the more efficient according to the benchmark tests:
-func GeneratePermutations_v3[T any](array []T) [][]T {
+func GeneratePermutations[T any](array []T) [][]T {
 	if len(array) == 1 {
 		return [][]T{array}
 	} else {
@@ -97,7 +19,7 @@ func GeneratePermutations_v3[T any](array []T) [][]T {
 				others[j] = array[i]
 				j++
 			}
-			subcombinations := GeneratePermutations_v3(others)
+			subcombinations := GeneratePermutations(others)
 			for _, subcombination := range subcombinations {
 				combination := append([]T{value}, subcombination...)
 				combinations = append(combinations, combination)
@@ -105,6 +27,24 @@ func GeneratePermutations_v3[T any](array []T) [][]T {
 		}
 		return combinations
 	}
+}
+
+func DeepCopySlice[T any](original []T) []T {
+	// Create a new slice with the same length as the original.
+	copied := make([]T, len(original))
+
+	// Copy each element from the original slice to the new slice.
+	for i, v := range original {
+		copied[i] = v
+	}
+
+	return copied
+}
+
+func ShallowCopySlice[T any](original []T) []T {
+	copied := make([]T, len(original))
+	copy(copied, original)
+	return copied
 }
 
 
@@ -115,4 +55,3 @@ func Flatten[T any](lists [][]T) []T {
     }
     return res
 }
-
--- util/arrays_test.go
+++ util/arrays_test.go
@@ -2,70 +2,244 @@ package util
 
 import (
 	"fmt"
+	"reflect"
+	"sort"
 	"testing"
 )
 
-/*
-user@computer:<escobita root path>$ go test -v ./util/ -bench=. -run=none -benchtime=10s
-goos: linux
-goarch: amd64
-pkg: github.com/vituchon/escobita/util
-cpu: Intel(R) Core(TM) i7-3630QM CPU @ 2.40GHz
-BenchmarkGeneratePermutations_v1a
-BenchmarkGeneratePermutations_v1a-8   	  100096	    118079 ns/op
-BenchmarkGeneratePermutations_v1b
-BenchmarkGeneratePermutations_v1b-8   	  104904	    123525 ns/op
-BenchmarkGeneratePermutations_v2
-BenchmarkGeneratePermutations_v2-8    	   94162	    128963 ns/op
-BenchmarkGeneratePermutations_v3
-BenchmarkGeneratePermutations_v3-8    	  101770	    113785 ns/op <-- FASTER
-PASS
-ok  	github.com/vituchon/escobita/util	53.371s
+func TestGeneratePermutationsWorks(t *testing.T) {
+	testRuns := []struct {
+		title    string
+		input    []int
+		expected [][]int
+	}{
+		{
+			title:    "Empty array",
+			input:    []int{},
+			expected: [][]int{},
+		},
+		{
+			title:    "2 element array",
+			input:    []int{1, 2},
+			expected: [][]int{[]int{1, 2}, []int{2, 1}},
+		},
+		{
+			title:    "3 element array",
+			input:    []int{1, 2, 3},
+			expected: [][]int{[]int{1, 2, 3}, []int{1, 3, 2}, []int{2, 1, 3}, []int{2, 3, 1}, []int{3, 1, 2}, []int{3, 2, 1}},
+		},
+		{
+			title:    "4 element array",
+			input:    []int{1, 2, 3, 4},
+			expected: [][]int{[]int{1, 2, 3, 4}, []int{1, 2, 4, 3}, []int{1, 3, 2, 4}, []int{1, 3, 4, 2}, []int{1, 4, 2, 3}, []int{1, 4, 3, 2}, []int{2, 1, 3, 4}, []int{2, 1, 4, 3}, []int{2, 3, 1, 4}, []int{2, 3, 4, 1}, []int{2, 4, 1, 3}, []int{2, 4, 3, 1}, []int{3, 1, 2, 4}, []int{3, 1, 4, 2}, []int{3, 2, 1, 4}, []int{3, 2, 4, 1}, []int{3, 4, 1, 2}, []int{3, 4, 2, 1}, []int{4, 1, 2, 3}, []int{4, 1, 3, 2}, []int{4, 2, 1, 3}, []int{4, 2, 3, 1}, []int{4, 3, 1, 2}, []int{4, 3, 2, 1}},
+		},
+	}
 
-go test -v ./util/ -bench=. -run=none -benchtime=10s
-goos: linux
-goarch: amd64
-pkg: github.com/vituchon/escobita/util
-cpu: Intel(R) Core(TM) i7-3630QM CPU @ 2.40GHz
-BenchmarkGeneratePermutations_v1a
-BenchmarkGeneratePermutations_v1a/slice.length:3
-BenchmarkGeneratePermutations_v1a/slice.length:3-8         	 3393565	      3685 ns/op
-BenchmarkGeneratePermutations_v1a/slice.length:4
-BenchmarkGeneratePermutations_v1a/slice.length:4-8         	  576556	     19936 ns/op
-BenchmarkGeneratePermutations_v1a/slice.length:5
-BenchmarkGeneratePermutations_v1a/slice.length:5-8         	   98236	    120050 ns/op
-BenchmarkGeneratePermutations_v1a/slice.length:6
-BenchmarkGeneratePermutations_v1a/slice.length:6-8         	   14029	    847624 ns/op
-BenchmarkGeneratePermutations_v1b
-BenchmarkGeneratePermutations_v1b/slice.length:3
-BenchmarkGeneratePermutations_v1b/slice.length:3-8         	 3205861	      3714 ns/op
-BenchmarkGeneratePermutations_v1b/slice.length:4
-BenchmarkGeneratePermutations_v1b/slice.length:4-8         	  555519	     20484 ns/op
-BenchmarkGeneratePermutations_v1b/slice.length:5
-BenchmarkGeneratePermutations_v1b/slice.length:5-8         	  101011	    123917 ns/op
-BenchmarkGeneratePermutations_v1b/slice.length:6
-BenchmarkGeneratePermutations_v1b/slice.length:6-8         	   13896	    866823 ns/op
-BenchmarkGeneratePermutations_v2
-BenchmarkGeneratePermutations_v2/slice.length:3
-BenchmarkGeneratePermutations_v2/slice.length:3-8          	 3149324	      3742 ns/op
-BenchmarkGeneratePermutations_v2/slice.length:4
-BenchmarkGeneratePermutations_v2/slice.length:4-8          	  529614	     20521 ns/op
-BenchmarkGeneratePermutations_v2/slice.length:5
-BenchmarkGeneratePermutations_v2/slice.length:5-8          	   90957	    127230 ns/op
-BenchmarkGeneratePermutations_v2/slice.length:6
-BenchmarkGeneratePermutations_v2/slice.length:6-8          	   13502	    872656 ns/op
-BenchmarkGeneratePermutations_v3
-BenchmarkGeneratePermutations_v3/slice.length:3
-BenchmarkGeneratePermutations_v3/slice.length:3-8          	 3432536	      3468 ns/op
-BenchmarkGeneratePermutations_v3/slice.length:4
-BenchmarkGeneratePermutations_v3/slice.length:4-8          	  632379	     18908 ns/op
-BenchmarkGeneratePermutations_v3/slice.length:5
-BenchmarkGeneratePermutations_v3/slice.length:5-8          	  102729	    115519 ns/op
-BenchmarkGeneratePermutations_v3/slice.length:6
-BenchmarkGeneratePermutations_v3/slice.length:6-8          	   14456	    798634 ns/op
+	for _, testRun := range testRuns {
+		t.Log("Running", testRun.title)
+		computed := GeneratePermutations(testRun.input)
+		if len(computed) != len(testRun.expected) {
+			t.Error("Length doesn't match, computed was: ", len(computed), " and expected is: ", len(testRun.expected))
+		}
+		if !HasSameValuesDisregardingOrder(computed, testRun.expected) {
+			t.Error("Computed was", computed, "and expected is", testRun.expected)
+		}
+	}
+}
 
-*/
-var slice []int = []int{1, 2, 3, 4, 5}
+func sortIntSliceSlice(slice [][]int) {
+	sort.Slice(slice, func(i, j int) bool {
+		iSlice := slice[i]
+		jSlice := slice[j]
+		for k := 0; k < len(iSlice); k++ {
+			if iSlice[k] < jSlice[k] {
+				return true
+			} else if iSlice[k] > jSlice[k] {
+				return false
+			}
+			// if they are equals then continue until there is a order difference or the slice ends
+		}
+		return false
+	})
+}
+
+func TestSortIntSliceSliceWork(t *testing.T) {
+	testRuns := []struct {
+		title    string
+		input    [][]int
+		expected [][]int
+	}{
+		{
+			title:    "Empty",
+			input:    [][]int{},
+			expected: [][]int{},
+		},
+		{
+			title:    "1 element (single, sorted)",
+			input:    [][]int{[]int{1}},
+			expected: [][]int{[]int{1}},
+		},
+		{
+			title:    "1 element (double, sorted)",
+			input:    [][]int{[]int{1, 2}},
+			expected: [][]int{[]int{1, 2}},
+		},
+		{
+			title:    "2 elements (single, sorted)",
+			input:    [][]int{[]int{1}, []int{2}},
+			expected: [][]int{[]int{1}, []int{2}},
+		},
+		{
+			title:    "2 elements (single, not sorted)",
+			input:    [][]int{[]int{2}, []int{1}},
+			expected: [][]int{[]int{1}, []int{2}},
+		},
+		{
+			title:    "2 elements (double, sorted)",
+			input:    [][]int{[]int{1, 2}, []int{2, 1}},
+			expected: [][]int{[]int{1, 2}, []int{2, 1}},
+		},
+		{
+			title:    "2 elements (double, not sorted)",
+			input:    [][]int{[]int{2, 1}, []int{1, 2}},
+			expected: [][]int{[]int{1, 2}, []int{2, 1}},
+		},
+		{
+			title:    "3 elements (triple, not sorted)",
+			input:    [][]int{[]int{4, 1, 1}, []int{1, 4, 5}, []int{1, 3, 5}},
+			expected: [][]int{[]int{1, 3, 5}, []int{1, 4, 5}, []int{4, 1, 1}},
+		},
+		{
+			title:    "4 elements (single, not sorted)",
+			input:    [][]int{[]int{2}, []int{5}, []int{1}, []int{3}},
+			expected: [][]int{[]int{1}, []int{2}, []int{3}, []int{5}},
+		},
+	}
+	for _, testRun := range testRuns {
+		t.Log("Running", testRun.title)
+		computed := DeepCopySlice(testRun.input)
+		sortIntSliceSlice(computed)
+		for i := 0; i < len(testRun.expected); i++ {
+			if !reflect.DeepEqual(computed[i], testRun.expected[i]) {
+				t.Error("Computed was", computed, "and expected is", testRun.expected)
+				break
+			}
+		}
+	}
+}
+
+func HasSameValuesDisregardingOrder(left, right [][]int) bool {
+	if len(left) != len(right) {
+		return false
+	}
+	sortIntSliceSlice(left)
+	sortIntSliceSlice(right)
+	return hasSameValuesRegardingOrder(left, right)
+}
+
+func hasSameValuesRegardingOrder(left, right [][]int) bool {
+	for i := 0; i < len(left); i++ {
+		if !reflect.DeepEqual(left[i], right[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+func TestHasSameValuesDisregardingOrder(t *testing.T) {
+	testRuns := []struct {
+		title    string
+		left     [][]int
+		right    [][]int
+		expected bool
+	}{
+		{
+			title:    "Empty",
+			left:     [][]int{},
+			right:    [][]int{},
+			expected: true,
+		},
+		{
+			title:    "1 element (single, equals)",
+			left:     [][]int{[]int{1}},
+			right:    [][]int{[]int{1}},
+			expected: true,
+		},
+		{
+			title:    "1 element (single, not equals: different values)",
+			left:     [][]int{[]int{1}},
+			right:    [][]int{[]int{2}},
+			expected: false,
+		},
+		{
+			title:    "1 element (single, not equals: different size)",
+			left:     [][]int{[]int{1}},
+			right:    [][]int{},
+			expected: false,
+		},
+		{
+			title:    "1 element (double, equals)",
+			left:     [][]int{[]int{1, 1}},
+			right:    [][]int{[]int{1, 1}},
+			expected: true,
+		},
+		{
+			title:    "1 element (double, not equals: different values)",
+			left:     [][]int{[]int{1, 1}},
+			right:    [][]int{[]int{1, 2}},
+			expected: false,
+		},
+		{
+			title:    "2 elements (single, equals, same order)",
+			left:     [][]int{[]int{1}, []int{2}},
+			right:    [][]int{[]int{1}, []int{2}},
+			expected: true,
+		},
+		{
+			title:    "2 elements (single, equals, different order)",
+			left:     [][]int{[]int{1}, []int{2}},
+			right:    [][]int{[]int{2}, []int{1}},
+			expected: true,
+		},
+		{
+			title:    "2 elements (single, not equals: different values)",
+			left:     [][]int{[]int{1}, []int{2}},
+			right:    [][]int{[]int{2}, []int{2}},
+			expected: false,
+		},
+		{
+			title:    "2 elements (single, not equals: different size)",
+			left:     [][]int{[]int{1}},
+			right:    [][]int{[]int{2}, []int{1}},
+			expected: false,
+		},
+		{
+			title:    "3 elements (triple, equals, same order)",
+			left:     [][]int{[]int{1, 3, 5}, []int{1, 4, 5}, []int{4, 1, 1}},
+			right:    [][]int{[]int{1, 3, 5}, []int{1, 4, 5}, []int{4, 1, 1}},
+			expected: true,
+		},
+		{
+			title:    "3 elements (triple, equals, different order)",
+			left:     [][]int{[]int{4, 1, 1}, []int{1, 4, 5}, []int{1, 3, 5}},
+			right:    [][]int{[]int{1, 3, 5}, []int{1, 4, 5}, []int{4, 1, 1}},
+			expected: true,
+		},
+		{
+			title:    "4 elements (single, equals, different order)",
+			left:     [][]int{[]int{2}, []int{5}, []int{1}, []int{3}},
+			right:    [][]int{[]int{1}, []int{2}, []int{3}, []int{5}},
+			expected: true,
+		},
+	}
+	for _, testRun := range testRuns {
+		t.Log("Running", testRun.title)
+		computed := HasSameValuesDisregardingOrder(testRun.left, testRun.right)
+		if computed != testRun.expected {
+			t.Error("Computed was", computed, "and expected is", testRun.expected)
+		}
+	}
+}
 
 var slices = [][]int{
 	[]int{1, 2, 3},
@@ -74,41 +248,11 @@ var slices = [][]int{
 	[]int{1, 2, 3, 4, 5, 6},
 }
 
-func BenchmarkGeneratePermutations_v1a(b *testing.B) {
-	for _, slice := range slices {
-		b.Run(fmt.Sprintf("slice.length:%v", len(slice)), func(b *testing.B) { // taking advice from  https://blog.logrocket.com/benchmarking-golang-improve-function-performance#Benchmarking with various inputs
-			for i := 0; i < b.N; i++ {
-				GeneratePermutations_v1a(slice)
-			}
-		})
-	}
-}
-
-func BenchmarkGeneratePermutations_v1b(b *testing.B) {
+func BenchmarkGeneratePermutations(b *testing.B) {
 	for _, slice := range slices {
 		b.Run(fmt.Sprintf("slice.length:%v", len(slice)), func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				GeneratePermutations_v1b(slice)
-			}
-		})
-	}
-}
-
-func BenchmarkGeneratePermutations_v2(b *testing.B) {
-	for _, slice := range slices {
-		b.Run(fmt.Sprintf("slice.length:%v", len(slice)), func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				GeneratePermutations_v2(slice)
-			}
-		})
-	}
-}
-
-func BenchmarkGeneratePermutations_v3(b *testing.B) {
-	for _, slice := range slices {
-		b.Run(fmt.Sprintf("slice.length:%v", len(slice)), func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				GeneratePermutations_v3(slice)
+				GeneratePermutations(slice)
 			}
 		})
 	}
