789417939aa4afa7b21cf118445a5acf0b9ea8e8
Vituchon <victor@omniasalud.com>
2024-08-20 12:20:46 -0300

    using web server

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/vituchon/escobita/presentation/web"
+	//"github.com/vituchon/escobita/presentation/cli"
 )
 
 func main() {
 	web.StartServer()
+	//cli.StartApp()
 }
